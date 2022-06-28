684e91a1b1fe184954cc2735e9598d7165e85746
Salwasalsabilaa <azhar.salwa.ft20@mail.umy.ac.id>
2022-06-28 13:59:35 +0700

    first commit

---
+++ projectakhir/main.js
@@ -0,0 +1,10 @@
+function tes(){
+    alert("Anda Akan dialihkan ke YouTube");
+}
+
+function newprompt(){
+    var name = prompt("Enter your name", "Enter name");
+    if (name != null) {
+    alert("Hello! "+ name)
+    }
+}
